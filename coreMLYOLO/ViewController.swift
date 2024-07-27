6cf60cd4945a2030a69b4fb4667184cb647b2d5a
yujin <northwindapps@gmail.com>
2024-07-27 22:59:15 +0900

    Initial Commit

---
+++ coreMLYOLO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreMLYOLO
+//
+//  Created by yano on 2024/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
