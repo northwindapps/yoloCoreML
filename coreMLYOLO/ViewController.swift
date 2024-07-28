15985bf2f2c92db4482eeb32e7292fc2254b3233
yujin <northwindapps@gmail.com>
2024-07-28 12:17:21 +0900

    in coding

--- coreMLYOLO/ViewController.swift
+++ coreMLYOLO/ViewController.swift
@@ -1,49 +1,64 @@
 import CoreML
 import Vision
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
     var model: VNCoreMLModel?
     var imageView: UIImageView!
+    var shapeLayers: [CAShapeLayer] = []
+    var textLayers: [CATextLayer] = []
+    var lastPredictionTime: Date = Date()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         loadModel()
-        guard let imageURL = Bundle.main.url(forResource: "billJP", withExtension: "jpg") else {
-            fatalError("Failed to find the image file.")
-        }
-        predictFromURL(imageURL)
+        setupCamera()
     }
-
-
 
     func setupUI() {
-            imageView = UIImageView(frame: view.bounds)
-            imageView.contentMode = .scaleAspectFit
-            view.addSubview(imageView)
-        }
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.zPosition = 1
+        view.addSubview(imageView)
+    }
 
     func loadModel() {
-        // Load Core ML model
-        guard let coreMLModel = try? VNCoreMLModel(for: best().model) else {
-            fatalError("Failed to load model")
-        }
-        self.model = coreMLModel
-    }
-
-    func predictFromURL(_ url: URL) {
-        guard let image = UIImage(contentsOfFile: url.path) else {
-            fatalError("Failed to create UIImage from file.")
-    }
-
-        DispatchQueue.global().async {
-            DispatchQueue.main.async {
-                self.imageView.image = image
-                self.predict(image: image)
+        do {
+            guard let modelURL = Bundle.main.url(forResource: "best", withExtension: "mlmodelc") else {
+                fatalError("Failed to find the model file.")
             }
+            let coreMLModel = try MLModel(contentsOf: modelURL)
+            self.model = try VNCoreMLModel(for: coreMLModel)
+        } catch {
+            fatalError("Failed to load model: \(error.localizedDescription)")
         }
+    }
+
+    func setupCamera() {
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+
+        guard let camera = AVCaptureDevice.default(for: .video) else { return }
+        let cameraInput = try? AVCaptureDeviceInput(device: camera)
+
+        if captureSession.canAddInput(cameraInput!) {
+            captureSession.addInput(cameraInput!)
+        }
+
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(videoOutput)
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
     }
 
     func predict(image: UIImage) {
@@ -73,38 +88,85 @@ class ViewController: UIViewController {
             return
         }
 
-        let imageSize = image.size
-        let imageViewSize = imageView.bounds.size
-        let scaleX = imageViewSize.width / imageSize.width
-        let scaleY = imageViewSize.height / imageSize.height
+        DispatchQueue.main.async {
+            self.clearPreviousShapes()
 
-        UIGraphicsBeginImageContextWithOptions(imageViewSize, false, 0.0)
-        image.draw(in: CGRect(origin: .zero, size: imageViewSize))
+            let imageSize = image.size
+            let imageViewSize = self.imageView.bounds.size
 
-        for observation in results {
-            let boundingBox = observation.boundingBox
-            let rect = CGRect(
-                x: boundingBox.origin.x * imageViewSize.width,
-                y: (1 - boundingBox.origin.y - boundingBox.height) * imageViewSize.height,
-                width: boundingBox.width * imageViewSize.width,
-                height: boundingBox.height * imageViewSize.height
-            )
+            for observation in results {
+                let boundingBox = observation.boundingBox
+                let rect = CGRect(
+                    x: boundingBox.origin.x * imageViewSize.width,
+                    y: (1 - boundingBox.origin.y - boundingBox.height) * imageViewSize.height,
+                    width: boundingBox.width * imageViewSize.width,
+                    height: boundingBox.height * imageViewSize.height
+                )
 
-            UIColor.red.setStroke()
-            UIRectFrame(rect)
+                let shapeLayer = CAShapeLayer()
+                shapeLayer.path = UIBezierPath(rect: rect).cgPath
+                shapeLayer.strokeColor = UIColor.red.cgColor
+                shapeLayer.lineWidth = 2.0
+                shapeLayer.fillColor = UIColor.clear.cgColor
 
-            if let label = observation.labels.first?.identifier {
-                let textRect = CGRect(x: rect.origin.x, y: rect.origin.y - 20, width: rect.size.width, height: 20)
-                let text = NSAttributedString(string: label, attributes: [.foregroundColor: UIColor.red])
-                text.draw(in: textRect)
+                self.imageView.layer.addSublayer(shapeLayer)
+                self.shapeLayers.append(shapeLayer)
+
+                if let label = observation.labels.first?.identifier {
+                    let textLayer = CATextLayer()
+                    textLayer.string = label
+                    textLayer.foregroundColor = UIColor.red.cgColor
+                    textLayer.fontSize = 14
+                    textLayer.frame = CGRect(x: rect.origin.x, y: rect.origin.y - 20, width: rect.size.width, height: 20)
+                    textLayer.contentsScale = UIScreen.main.scale
+                    self.imageView.layer.addSublayer(textLayer)
+                    self.textLayers.append(textLayer)
+                }
             }
         }
+    }
 
-        let annotatedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        DispatchQueue.main.async {
-            self.imageView.image = annotatedImage
+    func clearPreviousShapes() {
+        for layer in shapeLayers {
+            layer.removeFromSuperlayer()
         }
+        shapeLayers.removeAll()
+        
+        for layer in textLayers {
+            layer.removeFromSuperlayer()
+        }
+        textLayers.removeAll()
+    }
+
+    func convertCIImageToUIImage(ciImage: CIImage) -> UIImage? {
+        let context = CIContext(options: nil)
+        if let cgImage = context.createCGImage(ciImage, from: ciImage.extent) {
+            return UIImage(cgImage: cgImage)
+        }
+        return nil
+    }
+
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        let currentTime = Date()
+        if currentTime.timeIntervalSince(lastPredictionTime) < 3.0 {
+            return
+        }
+        lastPredictionTime = currentTime
+        
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        guard let uiImage = convertCIImageToUIImage(ciImage: ciImage) else {
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.predict(image: uiImage)
+        }
+        
     }
 }
+
